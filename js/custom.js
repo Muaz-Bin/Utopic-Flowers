ec7e54ca708df381e40772a5710d25ba8efdde5e
Muaz-Bin <64545414+Muaz-Bin@users.noreply.github.com>
2020-05-10 05:07:04 -0700

    Added all neccessery files

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".carousel").owlCarousel({
+    items:1,
+    autoplay:true,
+    autoplayTimeout:4000,
+    loop:true,
+    nav:true,
+    navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+    
+  });
+})
